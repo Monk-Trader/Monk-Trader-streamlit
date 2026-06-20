1dd3d6b5e3a7809c0127b2e285698396ffb78e96
Monk-Trader <rajeevk_ai@yahoo.com>
2026-06-21 00:22:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from ta.trend import ADXIndicator
 
 st.set_page_config(
     page_title="Monk Trader Quant Lab",
@@ -63,62 +64,16 @@ df.dropna(inplace=True)
 
 df['Feat_Return'] = df['Log_Return'].ewm(span=20).mean()
 df['Feat_Vol'] = df['Vol'].ewm(span=20).mean()
-# =====================================
-# ADX CALCULATION
-# =====================================
 
-period = 14
 
-high = df['High']
-low = df['Low']
-close = df['Close']
-
-plus_dm = high.diff()
-minus_dm = -low.diff()
-
-plus_dm = np.where(
-    (plus_dm > minus_dm) & (plus_dm > 0),
-    plus_dm,
-    0
+adx = ADXIndicator(
+    high=df['High'],
+    low=df['Low'],
+    close=df['Close'],
+    window=14
 )
 
-minus_dm = np.where(
-    (minus_dm > plus_dm) & (minus_dm > 0),
-    minus_dm,
-    0
-)
-
-tr1 = high - low
-tr2 = abs(high - close.shift())
-tr3 = abs(low - close.shift())
-
-tr = pd.concat(
-    [tr1, tr2, tr3],
-    axis=1
-).max(axis=1)
-
-atr = tr.rolling(period).mean()
-
-plus_di = (
-    100 *
-    pd.Series(plus_dm, index=df.index)
-    .rolling(period)
-    .mean() / atr
-)
-
-minus_di = (
-    100 *
-    pd.Series(minus_dm, index=df.index)
-    .rolling(period)
-    .mean() / atr
-)
-
-dx = (
-    abs(plus_di - minus_di) /
-    (plus_di + minus_di)
-) * 100
-
-df['ADX'] = dx.rolling(period).mean()
+df['ADX'] = adx.adx()
 
 # =====================================
 # REGIME ENGINE
