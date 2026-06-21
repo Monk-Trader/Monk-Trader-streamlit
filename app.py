c3d39a248c1ac29f0e662ee2ad5e2ec3a1fa75e7
Monk-Trader <rajeevk_ai@yahoo.com>
2026-06-21 14:38:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -471,8 +471,6 @@ company_col = strong_stocks.columns[0]
 symbol_col = "Symbol"
 
 strong_stocks["Display"] = (
-    strong_stocks[company_col].astype(str)
-    + " ("
     + strong_stocks[symbol_col].astype(str)
     + ")"
 )
