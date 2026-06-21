c53944b044a6be35483c2be55906fa48b24e5a45
Monk-Trader <rajeevk_ai@yahoo.com>
2026-06-21 14:01:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -456,34 +456,32 @@ st.header("🏆 Top Strong Stocks")
 st.markdown("---")
 st.header("📈 TradingView Chart")
 
+import streamlit.components.v1 as components
+
 components.html(
 """
 <div class="tradingview-widget-container">
   <div id="tradingview_chart"></div>
 
-  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-
-  <script type="text/javascript">
-  new TradingView.widget(
+  <script type="text/javascript"
+    src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+    async>
   {
-    "width": "100%",
-    "height": 600,
-    "symbol": "NSE:NIFTY",
+    "autosize": true,
+    "symbol": "NSE:SBIN",
     "interval": "D",
     "timezone": "Asia/Kolkata",
     "theme": "dark",
     "style": "1",
     "locale": "en",
-    "toolbar_bg": "#0E1117",
-    "enable_publishing": false,
     "allow_symbol_change": true,
-    "container_id": "tradingview_chart"
+    "calendar": false,
+    "support_host": "https://www.tradingview.com"
   }
-  );
   </script>
 </div>
 """,
-height=650,
+height=700,
 )
 
 strong_stocks = load_strong_stocks()
